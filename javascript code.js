2eed5e477df884d5ea7692adf63661813ad01e3d
KRISHNA KUMAR SHARMA <148979346+krishnasharma0329@users.noreply.github.com>
2024-04-11 13:02:35 +0530

    Add files via upload

---
+++ javascript code.js
@@ -0,0 +1,15 @@
+let display = document.getElementById('screen');
+function allclear() {
+    display.value = '';
+}
+function tambah() {
+    display.value += '-';
+}
+
+function show(n) {
+    display.value += n;
+}
+
+function calc() {
+    display.value = eval(display.value);
+}
